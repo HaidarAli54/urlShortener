ea50f561bdca964112ba339428f947e35c51d2a6
HaidarAli54 <haidarali5464@gmail.com>
2024-10-30 11:20:57 +0700

    Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from '../utils/jwt.config'
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from '../utils/prisma.service';
 
 @Module({
-    providers: [AuthService],
+    imports: [
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '60s' }
+        })
+    ],
+    providers: [AuthService, PrismaService, JwtStrategy],
     controllers: [AuthController]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/utils/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+    async register(username: string, email: string, password: string) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        return this.prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
+    }
+    async login(email: string, password: string) {
+        const user  = await this.prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new Error('Invalid password');
+        }
+        const payload = { email: user.email, sub: user.id };
+        return {
+            token: this.jwtService.sign(payload),
+            user,
+        };
+    }
+}
